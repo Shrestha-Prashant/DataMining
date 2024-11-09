a6a320786d8e413f77db0dd66494346c04e6699d
Shrestha-Prashant <prashantstha221117@gmail.com>
2024-11-09 15:32:24 +0545

    EDA I

--- EDA/main.py
+++ EDA/main.py
@@ -5,27 +5,27 @@ import matplotlib.pyplot as plt
 file_path = '/Users/panda/Downloads/training_and_development_data.csv'
 data = pd.read_csv(file_path)
 
-# # Performing an initial Overview
-# data.info()
-# print(data.head())
+# Performing an initial Overview
+data.info()
+print(data.head())
 
-# # Descriptive statistics for numerical columns
-# print(data.describe())
+# Descriptive statistics for numerical columns
+print(data.describe())
 
-# # Unique Value Analysis : trying to identify the variability within the data and potential patterns or categorical groupings
-# # getting unique values for each categorical column
-# unique_values = {col:data[col].unique() for col in data.select_dtypes(include=['object']).columns} 
-# print(unique_values)
+# Unique Value Analysis : trying to identify the variability within the data and potential patterns or categorical groupings
+# getting unique values for each categorical column
+unique_values = {col:data[col].unique() for col in data.select_dtypes(include=['object']).columns} 
+print(unique_values)
 
-# # Distribution of Training Duration
-# plt.figure(figsize=(12, 6))
-# plt.subplot(1, 2, 1)
-# sns.histplot(data['Training Duration(Days)'], bins=20, kde=True)
-# plt.title('Distribution of Training Duration')
-# plt.subplot(1, 2, 2)
-# sns.boxplot(x=data['Training Duration(Days)'])
-# plt.title('Boxplot of Training Duration')
-# plt.show()
+# Distribution of Training Duration
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+sns.histplot(data['Training Duration(Days)'], bins=20, kde=True)
+plt.title('Distribution of Training Duration')
+plt.subplot(1, 2, 2)
+sns.boxplot(x=data['Training Duration(Days)'])
+plt.title('Boxplot of Training Duration')
+plt.show()
 
 # Distribution of Training Cost
 plt.figure(figsize=(12, 6))
@@ -36,3 +36,16 @@ plt.subplot(1, 2, 2)
 sns.boxplot(x=data['Training Cost'])
 plt.title('Boxplot of Training Cost')
 plt.show()
+
+# Visualizing program effectiveness
+plt.figure(figsize=(14, 8))
+sns.countplot(data=data, y='Training Program Name', hue='Training Outcome',
+              order=data['Training Program Name'].value_counts().index)
+plt.title('Training Outcome Distribution by Program')
+plt.show()
+
+# cost comparison between training type 
+sns.boxplot(data=data, x='Training Type', y='Training Cost')
+plt.title('Training Cost by Type (Internal vs. External)')
+plt.show()
+
